310cf08d7ea0675f47d52e291cdb427e750c804c
taulmax <46551990+taulmax@users.noreply.github.com>
2020-06-28 23:03:53 +0900

    Axios로 API에서 정보를 가져옵니다.
    말 그대로 axios로 API에서 정보를 가져옵니다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "Loading..." : "We are ready!"}</div>
+    );
+  };
 }
 
 export default App;
