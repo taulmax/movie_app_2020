09a4309c2ef46c47ad398033fb6f29ad179dd591
taulmax <46551990+taulmax@users.noreply.github.com>
2020-07-29 21:37:48 +0900

    change
    change

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ class App extends React.Component {
               {movies.map(movie => (
                 <Movie
                   key={movie.id}
-                  id={movie.id}
                   year={movie.year}
                   title={movie.title}
                   summary={movie.summary}
-                  poster={movie.medium_cover_image} />
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres} />
               ))}
             </div>
           )}
--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Proptpyes from "prop-types";
 
-function Movie({ year, title, summary, poster }) {
-    return <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <p className="movie__summary">{summary}</p>
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary}</p>
+            </div>
         </div>
-    </div>
+    );
 }
 
 Movie.propTypes = {
@@ -17,7 +26,8 @@ Movie.propTypes = {
     year: Proptpyes.number.isRequired,
     title: Proptpyes.string.isRequired,
     summary: Proptpyes.string.isRequired,
-    poster: Proptpyes.string.isRequired
+    poster: Proptpyes.string.isRequired,
+    genres: Proptpyes.arrayOf(Proptpyes.string).isRequired
 }
 
 export default Movie;
